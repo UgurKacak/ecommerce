5da1a015106927f7450014bc3913cc2ea47ea26d
uğur <KAAK>
2021-03-09 18:36:26 +0300

    Swagger config edited for new package name.

--- src/main/java/com/ugur/config/SwaggerConfig.java
+++ src/main/java/com/ugur/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("ugurkacak.ecommerce.api.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.ugur.controller"))
                 .paths(PathSelectors.regex("/.*"))
                 .build()
                 .apiInfo(apiEndPointsInfo());
